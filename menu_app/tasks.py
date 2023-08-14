d910eceb35b30605c7db9e233d71e411af05e6de
IvanZhdaniuk <iz2819815@gmail.com>
2023-08-14 21:21:23 +0300

    created time decorator

--- menu_app/tasks.py
+++ menu_app/tasks.py
@@ -1,23 +1,36 @@
 import requests
 from bs4 import BeautifulSoup
 import fake_useragent
-import time
+import datetime
 
 from celery import shared_task
 
 latest_parse_data = None #глобальная переменная для хронения данных
 
 
+def time_of_function(func):
+    def wrapped(*args):
+        start_time = datetime.datetime.now()
+
+        res = func(*args)
+        stop_time = datetime.datetime.now()
+
+
+        print(f"Время парсинга сотовляет: {stop_time-start_time} секунд")
+        return res
+    return wrapped
+
 
 user = fake_useragent.UserAgent().random
 header = {'user.agent':user}
-
+@time_of_function
 @shared_task
 def do_parse():
-    global latest_parse_data
-    link = 'https://meteo.by/minsk/'
 
-    responce_bite = requests.get(link, headers=header).content
+    global latest_parse_data #Global variable for passing disparate data
+    # on the main page without using a database.
+    link = 'https://meteo.by/minsk/' #The address of the site from which we
+    # take weather data
 
     responce_text = requests.get(link, headers=header).text
 
@@ -32,6 +45,10 @@ def do_parse():
     latest_parse_data = {"check_js" :check_js, "check_js1" :check_js1}
     print(check_js)
     print(check_js1)
+    if latest_parse_data is None:
+        print("parsing don't work!!!!")
+
+
 
 # # with open('/home/user/Work/selfeducation/mens_food/parsing/static/parsing/1.html', 'w', encoding='utf-8') as file:
 # #     file.write(responce_text)
@@ -42,4 +59,8 @@ def do_parse():
 #         do_parse.delay()
 #         time.sleep(60)
 #
+
+
+
+
 v = do_parse()
